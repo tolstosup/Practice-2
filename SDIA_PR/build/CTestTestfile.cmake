69b49a3914632e029c2926c96a438524a66da374
roman@M1402 <demixhd2@gmail.com>
2024-11-05 20:48:16 +0300

    SDIA_PR2

--- SDIA_PR/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/Роман/Practice-2
-# Build directory: /mnt/c/Users/Роман/Practice-2/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("executable")
-subdirs("source")
